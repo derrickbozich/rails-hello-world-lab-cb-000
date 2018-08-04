dcd2ab8bbadcc89a4387871916c3621ba5414236
derrick b <dbozich1@gmail.com>
2018-08-04 05:58:08 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,6 @@
-get 'hello_world', to: 'static#hello_world'
+
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'hello_world', to: 'static#hello_world'
+end
